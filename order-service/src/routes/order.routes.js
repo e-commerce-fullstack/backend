2948b91caff5c31103084991920c1ca1a52bde4b
Oeun Nuphea <nupheaoeun@gmail.com>
2026-01-11 22:14:16 +0700

    update production payment

--- order-service/src/routes/order.routes.js
+++ order-service/src/routes/order.routes.js
@@ -1,10 +1,16 @@
 import { Router } from 'express'
-import { create, getAll, getById } from '../controllers/order.controller.js'
+import { create, getAll, getById, updateStatus } from '../controllers/order.controller.js' // Add updateStatus
 import { protect } from "../middlewares/protect.middleware.js";
 
 const router = Router()
 
-// All order routes should be protected
+/** * 1. WEBHOOK / INTERNAL ROUTE 
+ * This must be ABOVE router.use(protect) so the 
+ * Payment Service (Python) can call it.
+ */
+router.patch("/:id/confirm", updateStatus); 
+
+// --- All routes below this line require a User Login token ---
 router.use(protect); 
 
 router.post('/', create)
